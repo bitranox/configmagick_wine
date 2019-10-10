d47a53ea187111b78d3cc1db3f13d3f6c015ebb7
bitranox <rnowotny1966@gmail.com>
2019-10-10 15:34:39 +0200

    refractor

--- configmagick_wine/wine_install.py
+++ configmagick_wine/wine_install.py
@@ -28,6 +28,7 @@ def install_wine(wine_release: str, linux_release_name: str = configmagick_linux
     update_wine_packages()
     install_wine_packages(wine_release)
     install_winetricks()
+    update_winetricks()
 
 
 def raise_if_wine_release_unknown(wine_release: str) -> None:

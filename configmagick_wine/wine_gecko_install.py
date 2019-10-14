806d6c024f14f848ed310aadf269bf517fed0bdd
bitranox <rnowotny1966@gmail.com>
2019-10-14 16:13:43 +0200

    refractor

--- configmagick_wine/wine_gecko_install.py
+++ configmagick_wine/wine_gecko_install.py
@@ -34,15 +34,14 @@ def install_wine_gecko(wine_prefix: Union[str, pathlib.Path] = configmagick_linu
 
 def get_gecko_32_filename_from_appwiz(wine_prefix: pathlib.Path, username: str) -> pathlib.Path:
     """
-    >>> # wine_install.install_wine(wine_release='staging')
-    >>> # wine_prefix = configmagick_linux.get_path_home_dir_current_user() / '.wine'
-    >>> # username = configmagick_linux.get_current_username()
-    >>> username = 'consul'
-    >>> wine_prefix = pathlib.Path('home/consul/wine_test_32')
+    >>> wine_install.install_wine(wine_release='staging')
+    >>> wine_machine_install.install_wine_machine(wine_prefix='wine_test_32',wine_arch='win32')
+    >>> wine_machine_install.install_wine_machine(wine_prefix='wine_test_64',wine_arch='win64')
+    >>> username = configmagick_linux.get_current_username()
+    >>> wine_prefix = lib_wine.get_and_check_wine_prefix('wine_test_32', username=username)
     >>> get_gecko_32_filename_from_appwiz(wine_prefix, username)
-    >>> wine_prefix = pathlib.Path('home/consul/wine_test_64')
+    >>> wine_prefix = lib_wine.get_and_check_wine_prefix('wine_test_64', username=username)
     >>> get_gecko_32_filename_from_appwiz(wine_prefix, username)
-
 
     """
     wine_arch = lib_wine.get_wine_arch_from_wine_prefix(wine_prefix=wine_prefix, username=username)
@@ -51,25 +50,13 @@ def get_gecko_32_filename_from_appwiz(wine_prefix: pathlib.Path, username: str) 
     else:
         path_appwiz = wine_prefix / 'drive_c/windows/syswow64/appwiz.cpl'
 
-    gecko_32_filename = configmagick_linux.run_shell_command('strings -d --bytes=12 --encoding=s "{path_appwiz}" | grep wine_gecko | grep .msi'
-                                                             .format(path_appwiz=path_appwiz), shell=True, quiet=True)
+    if not path_appwiz.is_file():
+        raise RuntimeError('can not determine Gecko MSI Filename, File "{path_appwiz}" does not exist'.format(path_appwiz=path_appwiz))
+
+    response = configmagick_linux.run_shell_command('strings -d --bytes=12 "{path_appwiz}" | grep wine_gecko | grep .msi'
+                                                    .format(path_appwiz=path_appwiz), shell=True)
+    gecko_32_filename = response.stdout
     if not gecko_32_filename:
         raise RuntimeError('can not determine Gecko 32 Bit MSI File Name for WINEPREFIX="{wine_prefix}"'.format(wine_prefix=wine_prefix))
-    return pathlib.Path(str(gecko_32_filename))
-
-
-
-    path_appwiz = wine_prefix / 'drive_c/windows/system32/appwiz.cpl'
-    if not path_appwiz.is_file():
-        raise RuntimeError('can not determine Mono MSI Filename, File "{path_appwiz}" does not exist'.format(path_appwiz=path_appwiz))
-
-    # this fails from unknown reason on travis xenial !
-    response = configmagick_linux.run_shell_command('strings -d --bytes=12 --encoding=s "{path_appwiz}" | grep wine-mono | grep .msi'
-                                                    .format(path_appwiz=path_appwiz), shell=True, quiet=True)
-    mono_msi_filename = response.stdout
-
-    if not mono_msi_filename:
-        raise RuntimeError('can not determine Mono MSI Filename from WINEPREFIX="wine_prefix"'
-                           .format(wine_prefix=wine_prefix))
-    path_mono_msi = pathlib.Path(mono_msi_filename)
-    return path_mono_msi
+    gecko_32_filename = pathlib.Path(gecko_32_filename)
+    return gecko_32_filename

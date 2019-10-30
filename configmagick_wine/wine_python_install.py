c1e99c23c1196196d1478b27aea761e96884b843
bitranox <rnowotny1966@gmail.com>
2019-10-30 21:35:08 +0100

    refractor

--- configmagick_wine/wine_python_install.py
+++ configmagick_wine/wine_python_install.py
@@ -93,8 +93,8 @@ def install_wine_python(wine_prefix: Union[str, pathlib.Path] = configmagick_lin
                 display=configmagick_linux.get_env_display())
     """
 
-    command = 'xvfb-run WINEPREFIX="{wine_prefix}" WINEARCH="{wine_arch}" '\
-              'wineconsole "{wine_cache_directory}/{path_python_filename}" '\
+    command = 'WINEPREFIX="{wine_prefix}" WINEARCH="{wine_arch}" '\
+              'xvfb-run wineconsole "{wine_cache_directory}/{path_python_filename}" '\
               '/quiet InstallAllUsers=1 PrependPath=1 Include_test=0'\
         .format(wine_prefix=wine_prefix,
                 wine_arch=wine_arch,
